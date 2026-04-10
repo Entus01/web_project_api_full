acaa8e454f0c948316bfcb734be35b144313af43
Miguel Santiago <omungabarrera@gmail.com>
2026-04-10 17:15:00 -0500

    fix(routes): remove duplicate /users prefix in users router (paso 6)

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const {getUsers, getUserById, getCurrentUser, updateUser, updateAvatar } = require('../controllers/users');
 
 router.get('/', getUsers);
-router.get('/users/me', getCurrentUser);
-router.get('/users/:id', getUserById);
-router.patch('/users/me', updateUser);
-router.patch('/users/me/avatar', updateAvatar);
+router.get('/me', getCurrentUser);
+router.get('/:id', getUserById);
+router.patch('/me', updateUser);
+router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
